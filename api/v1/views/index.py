4b42cd89a232cec2ed3a667438eb758b9f9c3ed2
reinhard <reinhard2057@gmail.com>
2023-02-14 11:33:09 +0000

    task 3 still

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
-def index():
+@app_views.route('/status', , strict_slashes=False)
+def status():
     """blueprint function"""
     return jsonify({"status": "OK"})
