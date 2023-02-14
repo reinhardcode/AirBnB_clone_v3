ba366837daea778444b167cfca9d2f048677d9fe
reinhard <reinhard2057@gmail.com>
2023-02-14 11:50:19 +0000

    task 3 still

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ serve an api
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
@@ -20,7 +20,7 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     app.run(
-            host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')),
-            threaded=True
-            )
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=int(getenv('HBNB_API_PORT', '5000')),
+        threaded=True
+        )
