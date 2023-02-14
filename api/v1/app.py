45315b5b92037e59c4344065b86200c66096d52b
reinhard <reinhard2057@gmail.com>
2023-02-14 13:58:17 +0000

    task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,7 @@ def teardown_db(exception):
     """teardown functrion"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
