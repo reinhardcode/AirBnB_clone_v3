90f26331b6f638e4df8d2a99a7bb48e3061bb0de
reinhard <reinhard2057@gmail.com>
2023-02-14 11:45:50 +0000

    task 3 still

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,4 +19,8 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(
+            host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', '5000')),
+            threaded=True
+            )
